9460ca66e17475de2d73adab7d4e64e1e53ae4a4
Michelle Jordan <tmsmith1150@live.com>
2020-07-17 15:28:35 -0400

    completed delete

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -39,8 +39,41 @@ res.end()
 
   })
 
-  router.get("/api/notes", function(req, res) {
-    console.log("api call on notes")
+  router.get("/notes", function(req, res) {
+    
+    fs.readFile("./db/db.json","utf-8",function(err,data){
+      let dbObj=JSON.parse(data)
+
+      res.json(dbObj)
+    })
+
+
+  })
+
+  router.delete("/notes/:id",function(req,res){
+    let num=req.params.id
+
+   let fileData= fs.readFileSync("./db/db.json","utf-8")
+
+   let parseData = JSON.parse(fileData)
+
+   let newData= parseData.filter(item=>{
+     if(num != item.id){
+       return item
+     }
+   })
+
+   fs.writeFile("./db/db.json",JSON.stringify(newData),function(err){
+     if(err)throw err;
+
+     console.log("data deleted")
+     res.end()
+   })
+
+
+
+
+
 
 
   })
